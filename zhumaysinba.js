3b2eeac272c07f99d2f15cf6c8190af04f65bda6
Heorhii Siburov <g.siburov@gmail.com>
2020-07-23 16:42:17 -0700

    receiving the parameter from the console

---
+++ zhumaysinba.js
@@ -0,0 +1,10 @@
+let nodePath = process.argv[0];
+let appPath = process.argv[1];
+let bookPath = process.argv[2];
+
+console.log('+---==< Ololo >==---+')
+console.log("Node path: " + nodePath);
+console.log("App path: " + appPath);
+console.log();
+console.log("Book path: " + bookPath);
+console.log('---------------------')
