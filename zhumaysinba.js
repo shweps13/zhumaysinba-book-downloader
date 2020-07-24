9b20fbf0ad43621eccf3b84b856118f40b9d6943
Heorhii Siburov <g.siburov@gmail.com>
2020-07-23 19:39:08 -0700

    founded first script

--- zhumaysinba.js
+++ zhumaysinba.js
@@ -1,9 +1,10 @@
 const fs = require('fs-extra'); // files operations
 const scrape = require('website-scraper'); // web page scraper
 var rimraf = require("rimraf"); // directory removing
+var cheerio = require('cheerio'); // parser
 
 // Removing old temp if exist
-rimraf.sync("./temp1");
+rimraf.sync("./temp");
 
 const nodePath = process.argv[0];
 const appPath = process.argv[1];
@@ -21,13 +22,12 @@ console.log('---------------------')
 let fileTemp = './temp.txt'
 fs.openSync(fileTemp, 'w')
 
-
+let tempData = ''
 
 // Scrapper plugins
 class MyPlugin {
     apply(registerAction) {
         registerAction('beforeStart', async ({options}) => {console.log('==> Starting download secret zhumaysinba address from ', options.urls[0])});
-        // registerAction('beforeStart', async ({options}) => {rimraf("./temp1", function () { console.log("==> [Old temp removed]") })});
         registerAction('afterFinish', async () => {console.log('==> Done! We got book address!')});
         registerAction('error', async ({error}) => {console.error(error)});
     }
@@ -36,13 +36,37 @@ class MyPlugin {
 // Parameters for scraper
 const options = {
     urls: ['http://kazneb.kz/bookView/view/?brId=1557843&lang=kk'],
-    directory: './temp1',
+    directory: './temp',
     sources: [
-      {selector: 'script', attr: 'src'}
+      {selector: 'none', attr: 'none'}
     ],
     recursive: false,
     plugins: [ new MyPlugin() ]
   };
 
-scrape(options).then((result) => {console.log('-== Making zhumaysinba great again... ==-', '\n', '\n','---------------------')});
+const dataGet = () => {
+    // console.log(tempData)
+    fs.readFile('./temp/index.html', 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        // console.log(data);
+        // tempData = data
+        const $ = cheerio.load(data)
+        const pewpew = $('script')
+        console.log(pewpew.text())
+        console.log(pewpew.html())
+      });
+    
+    
+
+
+    console.log('ok, da')
+
+}
+
+scrape(options).then((result) => {tempData = result, console.log('-== Making zhumaysinba great again... ==-', '\n', '\n','---------------------'), dataGet()});
 console.log('')
+
+// We got the data on this moment...
+
