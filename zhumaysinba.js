9530ca4fa84ba87ec3a9651e1bf82dce56798243
Heorhii Siburov <g.siburov@gmail.com>
2020-07-23 17:57:32 -0700

    more work with page saving

--- zhumaysinba.js
+++ zhumaysinba.js
@@ -5,16 +5,26 @@ const nodePath = process.argv[0];
 const appPath = process.argv[1];
 let bookPath = process.argv[2];
 
+console.log('\n')
 console.log('+---==< Ololo >==---+')
 console.log("Node path: " + nodePath);
 console.log("App path: " + appPath);
 console.log();
-console.log(" ==> Book path: " + bookPath);
+console.log("==> Book path: " + bookPath);
 console.log('---------------------')
 
 // Creating or cleaning temp file for the page data
 let fileTemp = './temp.txt'
 fs.openSync(fileTemp, 'w')
+
+// Scrapper plugins
+class MyPlugin {
+    apply(registerAction) {
+        registerAction('beforeStart', async ({options}) => {console.log('==> Starting download secret zhumaysinba address from ', options.urls[0])});
+        registerAction('afterFinish', async () => {console.log('==> Done! We got book address!')});
+        registerAction('error', async ({error}) => {console.error(error)});
+    }
+    }
 
 // Parameters for scraper
 const options = {
@@ -23,23 +33,9 @@ const options = {
     sources: [
       {selector: 'script', attr: 'src'}
     ],
-    recursive: false
+    recursive: false,
+    plugins: [ new MyPlugin() ]
   };
 
-// Scrapper plugins
-class MyPlugin {
-apply(registerAction) {
-    registerAction('beforeStart', async ({options}) => {console.error('Starting download book from ', options.urls[0])});
-    registerAction('afterFinish', async () => {});
-    registerAction('error', async ({error}) => {console.error(error)});
-    registerAction('beforeRequest', async ({resource, requestOptions}) => ({requestOptions}));
-    registerAction('afterResponse', async ({response}) => response.body);
-    registerAction('onResourceSaved', ({resource}) => {});
-    registerAction('onResourceError', ({resource, error}) => {});
-    registerAction('saveResource', async ({resource}) => {});
-    registerAction('generateFilename', async ({resource}) => {})
-    registerAction('getReference', async ({resource, parentResource, originalReference}) => {})
-}
-}
-
-scrape(options).then((result) => {console.log(result)});
+scrape(options).then((result) => {console.log('-== Making zhumaysinba great again... ==-', '\n', '\n','---------------------')});
+console.log('')
